5de568f44066db0b0676f104dc452f39eee33697
Stacy <stacyxberlin@gmail.com>
2025-02-25 20:20:39 +0100

    complete tasks

---
+++ .idea/js/task-3.js
@@ -0,0 +1,36 @@
+class StringBuilder{
+    #value
+
+    constructor(initialValue){
+        this.#value = initialValue
+    }
+    getValue(){
+        return this.#value
+    }
+    padEnd(str){
+        this.#value = this.#value.split('')
+        this.#value.push(str)
+        this.#value = this.#value.join('')
+    }
+    padStart(str){
+        this.#value = this.#value.split('')
+        this.#value.unshift(str)
+        this.#value = this.#value.join('')
+    }
+    padBoth(str){
+        this.#value = this.#value.split('')
+        this.#value.push(str)
+        this.#value.unshift(str)
+        this.#value = this.#value.join('')
+    }
+}
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
